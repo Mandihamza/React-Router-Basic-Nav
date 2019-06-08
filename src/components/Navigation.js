4c285956bd8b03d12c68ce64a95671517fca5f74
mandihamza <mandihamza@gmail.com>
2019-06-08 11:21:37 -0600

    Fixed syntax and completed MVPs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,19 +7,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link to='/'>
-          <a href="">Home</a>
-          </Link>
+          <Link to='/'>Home</Link>
         </div>
         <div>
-        <Link to='/about'>          
-          <a href="">About</a>
-          </Link>
+        <Link to='/about'>About</Link>
         </div>
         <div>
-        <Link to='/contact'>
-          <a href="">Contact</a>
-          </Link>
+        <Link to='/contact'>Contact</Link>
         </div>
       </div>
     </div>
